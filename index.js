fc74a28470fb11396e5e708ca5200663736d79d8
Giulia Lima <giulialimaa210@gmail.com>
2025-06-26 15:17:13 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+var setaDireita = window.document.getElementById("seta-direita")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+
+
+function RolarParaDireita() {
+    leonardo.style = "display:none"
+    bruna.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex; margin-top: 30%"
+}
+
+function RolarParaEsquerda() {
+    leonardo.style = "display:flex"
+    bruna.style = "display:none"
+    setaDireita.style = "display:flex; margin-top: 30%"
+    setaEsquerda.style = "display:none"
+}
